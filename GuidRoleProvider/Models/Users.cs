5f7c07b6b6808e2567ed4865d8786defca8892e8
ctsinc <cts@heartlandfunds.com>
2013-11-05 10:23:21 -0600

    Files

---
+++ GuidRoleProvider/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuidRoleProvider.Models
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime LastActivityDate { get; set; }
+
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+}
---
+++ GuidRoleProvider/RoleProviderContext.cs
@@ -0,0 +1,16 @@
+﻿using GuidRoleProvider.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace GuidRoleProvider
+{
+    public class RoleProviderContext : DbContext
+    {
+        public RoleProviderContext() : base("RoleProviderContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
